3b424988d509df6e69ae56d4c41a7b6fb9a8e1a9
wreckless-omni <133142730+wreckless-omni@users.noreply.github.com>
2024-12-30 10:47:29 -0500

    Finished rep counter app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,224 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from 'react';
+import {Text, TextInput, View, Button, StyleSheet, Alert} from 'react-native';
+import { useState,useEffect} from 'react';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
-export default function HomeScreen() {
+
+
+
+const HelloWorldApp = () => {
+ 
+    const [squatCount, squatSetCount] = useState(0);
+    const [deadCount, deadSetCount] = useState(0);
+    const [benchCount, benchSetCount] = useState(0);
+    const [triCount, triSetCount] = useState(0);
+    const [curlCount, curlSetCount] = useState(0);
+    const [rowCount, rowSetCount] = useState(0);
+    const [setsNum, setNum] = useState(-1);
+    const legDay = ((squatCount + deadCount)/2);
+    const chestDay = ((benchCount + rowCount)/2);
+    const armsDay = ((curlCount + triCount)/2);
+    const wholeBody = ((squatCount + deadCount + benchCount + rowCount + curlCount + triCount) / 6)
+
+    useEffect(() => {
+      if (legDay === setsNum){Alert.alert('LEG DAY   IS DONE!!!')}
+    },[legDay]
+)
+
+useEffect(() => {
+  if (chestDay === setsNum){Alert.alert('CHEST DAY   IS DONE!!!')}
+},[chestDay]
+)
+
+useEffect(() => {
+  if (armsDay === setsNum){Alert.alert('ARMS DAY   IS DONE!!!')}
+},[armsDay]
+)
+ 
+
+useEffect(() => {
+  if (wholeBody === setsNum){Alert.alert('Whole Body   IS DONE!!! YOU ANIMAL!!!')}
+},[wholeBody]
+)
+     
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+
+    
+    <View
+
+
+    
+      style={{
+        padding:50,
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'stretch',
+        backgroundColor: 'firebrick',
+      }}>
+        
+    
+
+      <Text style={styles.titleText} > Pain is Glory!</Text>
+
+
+
+ {/*Text input box---------------------------------------------------------------------------------------------------------------*/}
+      <TextInput
+        style={{
+          height: 40,
+          padding: 10,
+          borderColor: 'gray',
+        
+          borderWidth: 1,
+        }}
+        placeholder="Enter the amount of sets, DO IT NOW!!! "
+        onChangeText={setzNum => setNum(Number.parseFloat(setzNum))}
+        keyboardType='number-pad'
+
+        
+      />
+    
+        
+
+
+      <Text style={styles.baseText} > --------------------Leg Day--------------------</Text>
+      
+      
+      {/*squats---------------------------------------------------------------------------------------------------------------*/}
+      <Text style = {styles.baseText}>Squats: {squatCount} sets </Text> 
+      <Button 
+      onPress={() => {squatSetCount(squatCount + 1)
+
+       
+      }} 
+      title="Squats"
+      color="red"
+      
+      />
+      
+      <Button
+        onPress={() => {squatSetCount( 0)}}
+        title = "Squat Reset"
+        color ="grey"
+
+
+/>
+
+
+      {/*deadlifts---------------------------------------------------------------------------------------------------------------------*/}
+      <Text style = {styles.baseText}>Deadlifts: {deadCount} sets </Text>
+      <Button 
+      onPress={() => {deadSetCount(deadCount + 1)}} 
+      title="Deadlifts"
+      color="goldenrod"
+      />
+      
+
+      <Button
+      onPress={() => {deadSetCount( 0)}}
+        title = "Deadlift Reset"
+        color = "grey"
+
+/>
+<Text style={styles.baseText} > ------------------Chest Day------------------</Text>
+{/*Benchpress---------------------------------------------------------------------------------------------------------------------*/}
+<Text style = {styles.baseText}>Benchpress: {benchCount} sets </Text>
+<Button 
+      onPress={() => {benchSetCount(benchCount + 1)}} 
+      title="Benchpress"
+      color="green"
+      
+      />
+      
+
+      <Button
+      onPress={() => {benchSetCount( 0)}}
+        title = "Benchpress Reset"
+        color = "grey"
+
+/>
+
+
+{/*Rows---------------------------------------------------------------------------------------------------------------------*/}
+<Text style = {styles.baseText}>Rows: {rowCount} sets </Text>
+<Button 
+      onPress={() => {rowSetCount(rowCount + 1)}} 
+      title="Rows"
+      color="dodgerblue"
+      
+      />
+      
+
+      <Button
+      onPress={() => {rowSetCount( 0)}}
+        title = "Row Reset"
+        color = "grey"
+
+/>
+
+
+<Text style={styles.baseText} > -----------------Arms Day-----------------</Text>
+{/*triceps---------------------------------------------------------------------------------------------------------------------*/}
+<Text style = {styles.baseText}>Triceps: {triCount} sets </Text>
+<Button 
+      onPress={() => {triSetCount(triCount + 1)}} 
+      title="Triceps"
+      color="indigo"
+      
+      />
+      
+
+      <Button
+      onPress={() => {triSetCount( 0)}}
+        title = "Triceps Reset"
+        color = "grey"
+
+/>
+
+{/*curls---------------------------------------------------------------------------------------------------------------------*/}
+<Text style = {styles.baseText}>Curls: {curlCount} sets </Text>
+<Button 
+      onPress={() => {curlSetCount(curlCount + 1)}} 
+      title="Curls"
+      color="blue"
+      
+      />
+     
+
+      <Button
+      onPress={() => {curlSetCount( 0)}}
+        title = "Curl Reset"
+        color = "grey"
+
+/>
+
+
+
+
+
+    </View>
+  
+
+    
   );
-}
+};
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  baseText: {
+    fontSize : 20,
+    textAlign: 'center'
+    
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+
+  titleText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign : 'center'
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+ 
+
 });
+
+
+export default HelloWorldApp;
